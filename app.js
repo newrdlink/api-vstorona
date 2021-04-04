aa0a475ba0b6c2d55c5f37784e53cbbbf9d4203c
newrdlink <newrdlink@gmail.com>
2021-04-04 10:14:09 +0300

    add structure

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const router = require('./router');
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
